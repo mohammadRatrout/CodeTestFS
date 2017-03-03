1e0a1a3d6f26dab80f43679d0e8c78fe7168aaa9
mohammadRatrout <mohammad.ratrout@gmail.com>
2017-03-03 15:33:31 +0200

    Step2(same i've decided to take more arguments) &step 3

--- Calculator/Calculator/Program.fs
+++ Calculator/Calculator/Program.fs
@@ -3,9 +3,13 @@
 let Add input =
     match input with
        |"" -> 0
-       |input-> input.Split(',') |> Array.map int |> Array.sum // first splitting the string it gave a character array 
+       |input-> input.Split(',','\n') |> Array.map int |> Array.sum // first splitting the string it gave a character array 
                                                               //in which we turn it to int then calculate the sum
-
+                                                              //for this code i will handle multible numbers as accepted value
 
 let resultOfEmpty= Add ""
-let result = Add "1,2" 
+let resultOfTwoOrMore = Add "1,2,4,5" 
+let resultOfNewLine=Add "1\n2,3"
+let resultOfNewLine2=Add "1,2\n3"//handling more opperands gave me the power of doing this
+                                 //in java i only allowed for two opperands and because of this  
+                                 //new line were only allowed before the delimeter
