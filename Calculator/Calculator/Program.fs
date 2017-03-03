95e6ee8968b366c378ddf9c8e94acff687ce2f5a
mohammadRatrout <mohammad.ratrout@gmail.com>
2017-03-03 11:40:05 +0200

    Step 1

---
+++ Calculator/Calculator/Program.fs
@@ -0,0 +1,11 @@
+﻿//Step 1
+
+let Add input =
+    match input with
+       |"" -> 0
+       |input-> input.Split(',') |> Array.map int |> Array.sum // first splitting the string it gave a character array 
+                                                              //in which we turn it to int then calculate the sum
+
+
+
+let result = Add "1,2" 
