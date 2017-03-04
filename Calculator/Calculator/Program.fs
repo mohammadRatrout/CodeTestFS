b792f0ab6d29f08cb6660f14a9d40478a1f435ef
mohammadRatrout <mohammad.ratrout@gmail.com>
2017-03-04 13:06:06 +0200

    Step5

--- Calculator/Calculator/Program.fs
+++ Calculator/Calculator/Program.fs
@@ -1,14 +1,21 @@
-﻿//Step 1,Step2,Step3
-
+﻿//Step 1,Step2,Step3,Step4,Step5
+open System// to use String.Join
 let Add input =
     match input with
        |"" -> 0
        |input when input.StartsWith("//")->
                 let delimeter=input.[2] 
-                input.[3..].Split(delimeter) |> Array.map int |>Array.sum
-       |input-> input.Split(',','\n') |> Array.map int |> Array.sum // first splitting the string it gave a character array 
-                                                              //in which we turn it to int then calculate the sum
-                                                              //for this code i will handle multible numbers as accepted value
+                let neg, pos = input.[3..].Split(delimeter) |> Array.map int |> Array.partition(fun n->n<0)
+                if neg.Length>0 then
+                invalidArg "input" (sprintf "the numbers are %s"<| String.Join(",",neg))
+                else
+                pos|>Array.sum
+       |input->  
+                let neg, pos = input.Split(',','\n') |> Array.map int |> Array.partition(fun n->n<0)
+                if neg.Length>0 then
+                invalidArg "input" (sprintf "the numbers are %s"<| String.Join(",",neg))//if there is negatives we do this
+                else
+                pos|>Array.sum   //else print them as usaual                                         
 
 let resultOfEmpty= Add ""
 let resultOfTwoOrMore = Add "1,2,4,5" 
@@ -18,3 +25,4 @@ let resultOfNewLine2=Add "1,2\n3"//handling more opperands gave me the power of 
                                  //new line were only allowed before the delimeter
 let resultOfDefinedDelimeter =Add "//;\n1;2"
 let resultOfDefinedDelimeter2 =Add "//#4\n#2#5"
+let resultOfNegatives = Add "//# 1#-2#-3"
